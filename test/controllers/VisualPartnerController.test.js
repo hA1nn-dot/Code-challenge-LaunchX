67fd0b737fb25409b167102e6787e4713ac780c0
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-05-02 18:39:12 -0500

    Create VisualPartnerController and testing

---
+++ test/controllers/VisualPartnerController.test.js
@@ -0,0 +1,18 @@
+const VisualPartnerController = require("./../../lib/controller/VisualPartnerController");
+describe("Testing VisualPartnerController", () => {
+    test("1) Get all info of students", () => {
+        const students = VisualPartnerController.getVisualPartners();
+        expect(students).toBeDefined();
+    });
+
+    test("2) Get emails of students if have certification", () => {
+        const studentsEmail = VisualPartnerController.getStudentsEmailByCertification();
+        expect(studentsEmail.length).toBe(51);
+    });
+
+    test("3) Get students above 500 credits", () => {
+        const credits = 500;
+        const studentsBycredits = VisualPartnerController.getStudentsaboveCredits(credits);
+        expect(studentsBycredits.length).toBe(27);
+    });
+})
